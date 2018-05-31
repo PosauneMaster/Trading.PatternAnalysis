fc7f86f74777650a2005a3df2fdce5377ec335d9
Bill Heine <whheine@gmail.com>
2018-05-31 14:59:24 -0500

    Add project files.

---
+++ Trading.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Trading.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ Trading.PatternAnalysis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trading.PatternAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
